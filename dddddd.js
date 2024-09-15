04f8ab47471758f02d7b3fa52e98ffe339c4700d
beapopro <116779584+Murad-Shaker@users.noreply.github.com>
2024-09-15 19:51:12 +0300

    Add files via upload

---
+++ dddddd.js
@@ -0,0 +1,74 @@
+ t1 = '2021.01.09-How to create a system like Facebook, the name can not be changed, except after a specific period of time using php';
+ t2 = '2021.02.10-System for displaying web content in your HTML page using php jquery';
+ t3 = '2021.02.18-How to create a view count system, using php and jquery';
+ t4 = '2021.04.05-Load Content while Scrolling with Jquery Ajax PHP';
+ t5 = '2021.05.08-How to increase the number of the variable every time the button is pressed using js';
+ t6 = '2021.05.09-Display the image directly when uploading itnfrom the input file using javascript';
+ t7 = '2021.06.15-how can i show user online or offline in php and ajax';
+ t8 = '2021.06.22-Writing clean, secure and easy to maintain PHP code';
+ t9 = '2021.06.29-PHP self Vs this';
+ t10 = '2021.06.30-How to show related posts similar to the current post USING PHP Mysqli';
+ t11 = '2021.07.04-PHP Pagination with Next Previous First Last page Link';
+ t12 = '2021.07.07-How do I insert a string, a text document, into mysqli using php';
+ t13 = '2021.07.12-How to insert multiple files (img mp3 mp4) into a mysqli file';
+ t14 = '2021.07.26-Simple Login System in Laravel';
+ t15 = '2021.07.30-How to call PHP function on the click of a Button';
+ t16 = '2021.08.04-How to create read more text in using php and jquery';
+ t17 = '2021.09.02-Authentication Login Register System In Laravel Laravel 8 For Beginners';
+ t18 = '2021.09.06-How to upload a video file to my database in Laravel 8';
+ t19 = '2021.10.01-Create a friend search system using php and ajax mysqli pdo';
+ t20 = '2022.04.21-Create a live chat system using php and jquery';
+ t21 = '2022.04.23-simple project How To Create a To Do List using javascript';
+ t22 = '2022.05.10-Create a simple calculator using Javascript';
+ t23 = '2022.06.05-How to make laravel 8 Shopping Cart';
+ t24 = '2022.06.06-How to check if a string contains a double space using javascript';
+ t25 = '2022.06.12-How to create a multilingual website using laravel';
+ t26 = '2022.06.14-View the most popular categories in order using JavaScript Jquery';
+ t27 = '2022.06.20-Create a fast live chat system without a database';
+ t28 = '2022.09.03-How to Create a Simple Notes Application with Vue.js 3';
+ t29 = '2022.09.19-Clarification and explanation of the maintenance mode in laravel';
+ t30 = '2022.10.12-How to upload books online without a database, and the visitor can download them';
+ t31 = '2022.12.07-How the team uses git and github for their workflow Detailed explanation';
+ t32 = '2023.01.12-How to build a piano keyboard using JavaScript in a simple way';
+ t33 = '2023.02.07-How to include an html file in another html file using javascript with an important problem solved';
+ t34 = '2023.03.26-Lesson for beginners Create a Digital Clock with Vue.js';
+ t35 = '2023.05.16-how to create login and register in Laravel with authentication and global middleware';
+
+ const arr = [t1,
+     t2,
+     t3,
+     t4,
+     t5,
+     t6,
+     t7,
+     t8,
+     t9,
+     t10,
+     t11,
+     t12,
+     t13,
+     t14,
+     t15,
+     t16,
+     t17,
+     t18,
+     t19,
+     t20,
+     t21,
+     t22,
+     t23,
+     t24,
+     t25,
+     t26,
+     t27,
+     t28,
+     t29,
+     t30,
+     t31,
+     t32,
+     t33,
+     t34,
+     t35
+ ];
+
+ arr.reverse()
